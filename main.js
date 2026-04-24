d40eee560185b2fbc42541b0d0754605ca1af3fa
Aakashraj24 <129396513+Aakashraj24@users.noreply.github.com>
2026-04-24 07:03:37 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+goup_logo = "img/goup.png" // "https://iili.io/d642heR.png"
+gky_logo = "img/logo.png" // "https://iili.io/d64fmMB.png"
+profile_imgae = "img/org.jpg" // "https://raw.githubusercontent.com/Aakashraj24/Ration-Card-Up-Hindi-/refs/heads/main/img/Picsart_25-05-22_16-45-38-787.jpg?token=GHSAT0AAAAAADC4KLFPO2QKVK5SZYPQ7DEA2BPBA6A"
+call_logo = "svgs/call.svg" // "https://site-assets.fontawesome.com/releases/v6.6.0/svgs/solid/phone.svg"
+web_logo = "svgs/globe.svg" // "https://site-assets.fontawesome.com/releases/v6.6.0/svgs/light/globe.svg"
+email_logo = "svgs/email_4.svg" // "https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/email-icon.svg"
+
+
+// Goverments of Uttar Pradesh : logo
+goup_icon = document.querySelector('.flimg')
+goup_icon.src = goup_logo
+// nsnnsnsn Second
+gky_icon = document.querySelector('.llimg')
+gky_icon.src = gky_logo
+
+goup_icon2 = document.querySelector(".fl2img")
+goup_icon2.src = goup_logo
+
+gky_icon2 = document.querySelector('.ll2img')
+gky_icon2.src = gky_logo
+
+// profile photo
+profile_photo = document.querySelector('.photo')
+profile_photo.src = profile_imgae
+
+// call icon
+call_icon = document.querySelector('.fimg')
+call_icon.src = call_logo
+
+// website logo : globe
+web_icon = document.querySelector('.wiimg')
+web_icon.src = web_logo
+
+// email icon
+email_icon = document.querySelector('.eimg')
+email_icon.src = email_logo
+
+// email icon 2
+email_icon2 = document.querySelector('.sm2im')
+email_icon2.src = email_logo
